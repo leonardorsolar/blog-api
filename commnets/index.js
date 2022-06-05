42676a851b264901ff4b3e8815cd6e705662714f
leonardosolar <leonardosolar@yahoo.com.br>
2022-06-04 23:45:26 -0300

    first commit

---
+++ commnets/index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const { randomBytes } = require('crypto')
+const cors = require('cors')
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+const port = 4001
+
+
+const commentsByPostId = {}
+
+app.get('/posts/:id/comments',  (req, res) => {
+  res.send( commentsByPostId[req.params.id] || [])
+})
+
+app.post('/posts/:id/comments',  (req, res) => {
+    const commentId = randomBytes(4).toString('hex')
+    const { content } = req.body
+
+    const comments = commentsByPostId[req.params.id] || []
+    
+    comments.push({ id: commentId, content})
+
+    commentsByPostId[req.params.id] = comments
+
+    res.status(201).send(comments)
+})
+
+app.listen(port, () => {
+  console.log(`[App]: Listening on port ${port}`)
+})
+
+// criamos um objeto
---
+++ posts/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const { randomBytes } = require('crypto')
+const cors = require('cors')
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+const port = 4000
+
+const posts = {}
+
+app.get('/posts',  (req, res) => {
+
+  res.send(posts)
+  console.log(posts)
+  
+})
+
+app.post('/posts',  (req, res) => {
+    const id = randomBytes(4).toString('hex')
+    const { title } = req.body
+
+    posts[id] = {
+      id, title
+    }
+
+    res.status(201).send(posts[id])
+})
+
+app.listen(port, () => {
+  console.log(`[App]: Listening on port ${port}`)
+})
